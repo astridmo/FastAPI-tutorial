9b3d28a26d19fbcde60abdf863eaa416ec714683
astridmo <astrid.moum@gmail.com>
2022-07-12 10:14:25 +0200

    use depends instead of with

--- todo.py
+++ todo.py
@@ -10,7 +10,7 @@ __email__ = 'astridmo@nmbu.no'
 
 from typing import List, Optional
 
-from fastapi import FastAPI, HTTPException, Query
+from fastapi import Depends, FastAPI, HTTPException, Query
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 
 
@@ -50,6 +50,11 @@ def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
 
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
 app = FastAPI()
 
 
@@ -59,57 +64,53 @@ def on_startup():
 
 
 @app.post("/todo/", response_model=ToDoRead)
-def create_todo(todo: ToDoCreate):
-    with Session(engine) as session:
-        db_todo = ToDo.from_orm(todo)
-        session.add(db_todo)
-        session.commit()
-        session.refresh(db_todo)
-        return db_todo
+def create_todo(*, session: Session = Depends(get_session), todo: ToDoCreate):
+    db_todo = ToDo.from_orm(todo)
+    session.add(db_todo)
+    session.commit()
+    session.refresh(db_todo)
+    return db_todo
 
 
 @app.get("/todo/", response_model=List[ToDo])
-def read_todos(offset: int = 0, limit: int = Query(default=100, lte=100)):
+def read_todos(*, session: Session = Depends(get_session),
+               offset: int = 0, limit: int = Query(default=100, lte=100)):
     """
     Read todos.
     Returns the first results from database (offset=0), and a maximum of 100 todos (limit 100)
     """
-    with Session(engine) as session:
-        todos = session.exec(select(ToDo).offset(offset).limit(limit)).all()
-        return todos
+    todos = session.exec(select(ToDo).offset(offset).limit(limit)).all()
+    return todos
 
 
 @app.get("/todo/{todo_id}", response_model=ToDoRead)
-def read_todo(todo_id: int):
+def read_todo(*, session: Session = Depends(get_session), todo_id: int):
     """Read hero based on hero_id"""
-    with Session(engine) as session:
-        todo = session.get(ToDo, todo_id)
-        if not todo:
-            raise HTTPException(status_code=404, detail="Todo not found")
-        return todo
+    todo = session.get(ToDo, todo_id)
+    if not todo:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    return todo
 
 
-@app.patch("/heroes/{hero_id}", response_model=ToDoRead)
-def update_hero(todo_id: int, todo: ToDoUpdate):
-    with Session(engine) as session:
-        db_todo = session.get(ToDo, todo_id)
-        if not db_todo:
-            raise HTTPException(status_code=404, detail="Todo not found")
-        todo_data = todo.dict(exclude_unset=True)
-        for key, value in todo_data.items():
-            setattr(db_todo, key, value)
-        session.add(db_todo)
-        session.commit()
-        session.refresh(db_todo)
-        return db_todo
+@app.patch("/todo/{todo_id}", response_model=ToDoRead)
+def update_hero(*, session: Session = Depends(get_session), todo_id: int, todo: ToDoUpdate):
+    db_todo = session.get(ToDo, todo_id)
+    if not db_todo:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    todo_data = todo.dict(exclude_unset=True)
+    for key, value in todo_data.items():
+        setattr(db_todo, key, value)
+    session.add(db_todo)
+    session.commit()
+    session.refresh(db_todo)
+    return db_todo
 
 
-@app.delete("/heroes/{hero_id}")
-def delete_todo(todo_id: int):
-    with Session(engine) as session:
-        todo = session.get(ToDo, todo_id)
-        if not todo:
-            raise HTTPException(status_code=404, detail="Todo not found")
-        session.delete(todo)
-        session.commit()
-        return {"ok": True}
+@app.delete("/todo/{todo_id}")
+def delete_todo(*, session: Session = Depends(get_session), todo_id: int):
+    todo = session.get(ToDo, todo_id)
+    if not todo:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    session.delete(todo)
+    session.commit()
+    return {"ok": True}
