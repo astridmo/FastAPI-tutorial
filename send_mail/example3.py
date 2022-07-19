9c67c4ff514540c9e069241f975ee2d8b07ac149
astridmo <astrid.moum@gmail.com>
2022-07-19 17:20:54 +0200

    clean up

--- send_mail/example3.py
+++ send_mail/example3.py
@@ -2,51 +2,50 @@
 # -*- coding: utf-8 -*-
 
 """
-...information about the code...
+Send attachment (image or PDF. Some code must be changed (the not used lines are commented out).
+Send to several receivers.
 """
 
 __author__ = 'Astrid Moum'
 __email__ = 'astridmo@nmbu.no'
 
-# Import smtplib for the actual sending function
 import smtplib, ssl
-
-# And imghdr to find the types of our images
 import imghdr
-
-# Here are the email package modules we'll need
 from email.message import EmailMessage
-from PIL import Image
 import glob
-image_list = []
-for filename in glob.glob('./assets/logo_alwayscargo.png'): #assuming gif
-    im=Image.open(filename)
-    image_list.append(im)
-    print(image_list)
 
-sender_email = "oslo_venner@hotmail.com"
-family = ["testuser4development@gmail.com"]
 smtp_server = "smtp.office365.com"
-password = "vennene"
 port = 587  # For starttls
+#attachment_path = './assets/logo_alwayscargo.png'  #picture
+attachment_path = './assets/test_pdf.pdf'
 
-# Create the container email message.
-msg = EmailMessage()
+sender_email = "oslo_venner@hotmail.com"  # sender's email address
+family = ["testuser4development@gmail.com"]  # List of of all recipients' email addresses
+password = "vennene"
+
+
+# Open the plain text file whose name is in textfile for reading.
+with open("textfile.txt") as fp:
+    # Create a text/plain message
+    msg = EmailMessage()
+    msg.set_content(fp.read())
+
+
 msg['Subject'] = 'Our family reunion'
-# me == the sender's email address
-# family = the list of all recipients' email addresses
 msg['From'] = sender_email
 msg['To'] = ', '.join(family)
 msg.preamble = 'You will not see this in a MIME-aware mail reader.\n'
 
 # Open the files in binary mode.  Use imghdr to figure out the
 # MIME subtype for each specific image.
-for file in glob.glob('./assets/logo_alwayscargo.png'):
+for file in glob.glob(attachment_path):
     print(file)
     with open(file, 'rb') as fp:
-        img_data = fp.read()
-    msg.add_attachment(img_data, maintype='image',
-                                 subtype=imghdr.what(None, img_data))
+        # img_data = fp.read()  # for attaching image
+        pdf_data = fp.read()  # for attaching pdf
+    # msg.add_attachment(img_data, maintype='image', subtype=imghdr.what(None, img_data))  # for attaching image
+    msg.add_attachment(pdf_data, maintype='application', subtype='pdf', filename='example.pdf')  # for attaching PDF
+
 
 context = ssl.create_default_context()
 # Send the email via our own SMTP server.
